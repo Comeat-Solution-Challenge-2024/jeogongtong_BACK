4aa347432d1dcee0c14f56905a57c170724d0dff
jylee <juyeon01530@gmail.com>
2024-02-15 02:12:24 +0900

    마이페이지 스터디/ 랭킹 업로드

--- src/main/java/com/comeat/jeogongtong/member/controller/MemberController.java
+++ src/main/java/com/comeat/jeogongtong/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.comeat.jeogongtong.member.controller;
 
 import com.comeat.jeogongtong.member.dto.MemberDto;
 import com.comeat.jeogongtong.member.service.MemberService;
+import com.comeat.jeogongtong.study_member.dto.StudyMemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,15 +10,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
+
+    /* 마이페이지 조회*/
     @GetMapping("/{memberId}")
     public String findById(@PathVariable Long memberId, Model model){
         MemberDto memberDto = memberService.findById(memberId);
         model.addAttribute("member",memberDto);
+
+        //스터디 랭킹 추가
+        List<StudyMemberDto> studyRank = memberService.findStudyRankByMemberId(memberId);
+        model.addAttribute("studyRank", studyRank);
         return "my_page";
     }
 }
--- src/main/java/com/comeat/jeogongtong/member/service/MemberService.java
+++ src/main/java/com/comeat/jeogongtong/member/service/MemberService.java
@@ -3,16 +3,21 @@ package com.comeat.jeogongtong.member.service;
 import com.comeat.jeogongtong.member.dto.MemberDto;
 import com.comeat.jeogongtong.member.entity.MemberEntity;
 import com.comeat.jeogongtong.member.repository.MemberRepository;
+import com.comeat.jeogongtong.study_member.dto.StudyMemberDto;
+import com.comeat.jeogongtong.study_member.entity.StudyMemberEntity;
+import com.comeat.jeogongtong.study_member.repository.StudyMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
-
+    private final StudyMemberRepository studyMemberRepository;
     public MemberDto login(MemberDto memberDto) {
         Optional<MemberEntity> byMemberEmail = memberRepository.findByEmail(memberDto.getEmail());
         if (byMemberEmail.isPresent()){
@@ -33,5 +38,13 @@ public class MemberService {
 
     public Optional<MemberEntity> findMemberEntityById(Long memberId) {
         return memberRepository.findById(memberId);
+    }
+
+    /*사용자 스터디 랭킹 조회 -> 마이페이지에 랭킹 출력*/
+    public List<StudyMemberDto> findStudyRankByMemberId(Long memberId){
+        List<StudyMemberEntity> studyMember = studyMemberRepository.findByMember_MemberId(memberId);
+        return studyMember.stream()
+                .map(StudyMemberDto::toStudyMemberDto)
+                .collect(Collectors.toList());
     }
 }
